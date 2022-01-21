b7955caedcefa56ec6c2631ed59a3a6f61cdf6e0
Oleysys <oleysys@gmail.com>
2022-01-21 12:35:04 -0300

    Static File
    Static File  and index

--- static/configuration/config.js
+++ static/configuration/config.js
@@ -1,20 +1,20 @@
 window.globalConf = {
-  _welcomePage: 'static/configuration/loc/en/html/howto.html',
+  welcomePage: 'static/configuration/loc/en/html/howto.html',
   languages: [
     { id: 'en', 'label': 'English' },
-    { id: 'fr', 'label': 'Français' }
+    { id: 'nl', 'label': 'Nederlands' },
+    { id: 'srn', 'label': 'Sranan Tongo' }
   ],
   map: {
     bingMapsKey: 'ArXO9BbbCsJXDlX8dLLxcI5BhFPTPrKBdg9o-BNWvC8kJFTetsc96izVu0415SDy',
-    mapCenterLonLat: [21.7587, -4.0383],
-    mapZoom: 5,
+    mapCenterLonLat: [-56.6235353, 4.008795],
+    mapZoom: 7,
     defaultGeoServerURLs: [
-      'http://rdc-snsf.org/diss_geoserver/wms',
-      'http://178.33.8.115/diss_geoserver/wms'
+      'https://gonini.org/geoserver/wms'
     ]
   },
   layersConfigApi: {
-    baseUrl: '/api/',
+    baseUrl: 'api/',
     loginUrl: 'sessions/create',
     getLayersConfigUrl: 'layers.json',
     saveLayersConfigUrl: 'protected/layers_conf/save/',
@@ -22,35 +22,39 @@ window.globalConf = {
     restoreVersionUrl: 'protected/layers_conf/restore_version'
   },
   feedbackApi: {
-    askForEmail: false,
-    feedbackUrl: 'http://localhost:3001/feedback',
-    feedbackCategories: [
-      'Thematic mapping and Monitoring',
-      'Forest Offences',
-      'Information Dissemination (RTI)'
-    ]
-  },
-  printApi: {
-    url: 'http://localhost:8080/geoserver/pdf/print.pdf'
+    askForEmail: true,
+    feedbackUrl: 'feedback',
+    drawOptions: [
+      'Point'
+    ],
+    drawDefault: 'Point'
   },
   logos: {
     ul: 'flag.gif',
     ll: [
       {
-        imgUrl: '/static/configuration/logos/unredd.png',
+        imgUrl: 'static/configuration/logos/unredd.png',
         link: 'http://www.unredd.net/',
         size: { height: 35 }
       },
       {
-        imgUrl: '/static/configuration/logos/inpe.png',
-        link: 'http://www.inpe.br/ingles/',
+        imgUrl: 'static/configuration/logos/reddPluslogo.png',
+        link: 'https://www.surinameredd.org/',
         size: { height: 35 }
       },
       {
-        text: 'Text link',
-        link: 'http://uxmyths.com/post/715009009/myth-icons-enhance-usability',
+        text: 'send us your feedback: gonini.suriname@gmail.com',
+        link: 'https://www.facebook.com/Goninigeoportaal/',
         size: { height: 35 }
       }
     ]
+  },
+  wfsSearchConfig: {
+    layersToQuery: ['gonini:Infrastructuur_14092021', 'gonini:terreinen', 'gonini:settlements', 'gonini:Hydrography_Name', 'gonini:terreinen'],
+    attributesToFilter: ['str_naam', 'cc_code', 'name', 'name', 'naam'],
+    baseURL: 'geoserver/ows'
+  },
+  printApi: {
+    url: '/geoserver/pdf/print.pdf'
   }
 }
